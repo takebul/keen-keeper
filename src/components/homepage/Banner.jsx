5431a1b9439b9b710df440d35c1da7e6639424a5
takebul <takebulislam@gmail.com>
2026-04-19 06:50:37 +0600

    update banner component

--- src/components/homepage/Banner.jsx
+++ src/components/homepage/Banner.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>This is Banner Section</h2>
+    <div className="w-11/12 mx-auto text-center my-10 space-y-3">
+      <h1 className="font-bold text-[40px]">
+        Friends to keep close in your life
+      </h1>
+      <p className="font-light text-gray-600 text-sm">
+        Your personal shelf of meaningful connections. Browse, tend, and nurture
+        the <br /> relationships that matter most.
+      </p>
+      <button className="btn bg-green-800 text-white">+ Add a Friend</button>
     </div>
   );
 };
